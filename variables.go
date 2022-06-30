0e6a98f7b10f779336e9dd2d41e2ac1350567efd
alvo254 <alvinkhamron@gmail.con>
2022-06-30 15:46:14 +0300

    variables

--- variables.go
+++ variables.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
-
 //variable can be defined with var or const keywords
-
+//this is global scope
+var i float64 = 21.3
+var j int = 43
 
 func main(){
-	
+	fmt.Printf("%v %v", i, j)
+
+
 	fmt.Println("Welcome to ge")
 }
